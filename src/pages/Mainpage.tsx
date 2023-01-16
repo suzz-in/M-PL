962b20e17098e0d3f07003493f76a42adfff75ad
MIINII <ft.dawn.o@gmail.com>
2023-01-16 17:28:00 +0900

    Style : tailwind 설정

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,10 +1,9 @@
 const Mainpage = () => {
   return (
     <>
-      <h1>mpl</h1>
+      <h1 className="text-xl text-mpl_pink">mpl</h1>
       <nav>
         <img src="../assets/nav.png" alt="네비게이션바" />
-        
       </nav>
     </>
   );
