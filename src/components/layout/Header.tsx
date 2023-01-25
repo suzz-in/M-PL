4b6d5b4512aa4fc304be63dc4362b893c625a3f2
simyuseon <yseon0508@naver.com>
2023-01-26 02:56:40 +0900

    Design: 헤더 view

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="flex justify-between ml-4 mr-4 m-8">
+      <h1>
+        <img src="logo.svg" alt="앰플 로고" />
+      </h1>
+      <img src="nav.png" alt="hambugerButton" />
+    </header>
+  );
+};
+
+export default Header;
